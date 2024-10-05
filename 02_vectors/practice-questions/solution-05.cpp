d3e9e677a8bf34ab6a82d7119483ea0527145ae6
Anuj Das <anonboy1434@gmail.com>
2024-10-06 00:44:41 +0530

    Create solution-05.cpp

---
+++ 02_vectors/practice-questions/solution-05.cpp
@@ -0,0 +1,25 @@
+// 5. Given a vector of integers, write a function to remove 
+//    all duplicate elements from the vector and return the updated vector.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(vector<int> list, string caption) {
+    cout << caption;
+    for(int x: list) {
+        cout << x << ", ";
+    }
+    cout << endl;
+} 
+
+vector<int> removeDuplicates(vector<int> arr) {
+    vector<int> temp;
+
+    return temp;
+}
+
+int main() {
+    vector<int> v{1, 2, 3, 3, 1, 1, 2, 5, 2};
+    print(v, "vector<int> v = ");
+    return 0;
+}
